7afa44cbfd4593b92835f2ba284b0b2afecb73d7
Valery Shcherbinin <fundroin@mail.ru>
2022-06-17 00:25:09 +0500

    добавляет слайдер swipper и стили для элеметнов слайдера

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,11 +1,22 @@
-import Swiper from 'swiper';
-
 document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.swiper', {
-        direction: 'vertical',
-        loop: true,
+        direction: 'horizontal',
+        loop: false,
         pagination: {
             el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            1174: {
+                slidesPerView: 3,
+                slidesPerGroup: 1,
+                spaceBetween: 30
+            },
+            425: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+                centeredSlides: true
+            }
         }
     });
 });
